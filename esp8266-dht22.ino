8016711a9f9be7803e4afe2fc1fc2cccc2a87d92
Lev Petrushchak <sabretus@gmail.com>
2017-01-03 00:27:59 +0200

    Update esp8266-dht22.ino

--- esp8266-dht22.ino
+++ esp8266-dht22.ino
@@ -1,161 +1,105 @@
-/*
- Created by Igor Jarc
- See http://iot-playground.com for details
- Please use community fourum on website do not contact author directly
- 
- External libraries:
- DTH - https://github.com/iot-playground/EasyIoT-Cloud/tree/master/libraries/DHT
- 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
- */
+// ESP8266 DS18B20 ArduinoIDE Thingspeak IoT Example code
+// http://vaasa.hacklab.fi
+//
+// https://github.com/milesburton/Arduino-Temperature-Control-Library
+// https://gist.github.com/jeje/57091acf138a92c4176a
+
 #include <ESP8266WiFi.h>
-#include "DHT.h"
+#include <ESP8266WebServer.h>
 
-//AP definitions - update this
-#define AP_SSID     ""
-#define AP_PASSWORD ""
+#include <DHT.h>
+#define DHTTYPE DHT22  //or DHT11
+#define DHTPIN  2 // GPIO 2
+#define REPORT_INTERVAL 30 //in sec
 
-// EasyIoT Cloud definitions - change EIOT_CLOUD_TEMP_INSTANCE_PARAM_ID and EIOT_CLOUD_HUM_INSTANCE_PARAM_ID
-#define EIOT_CLOUD_TEMP_INSTANCE_PARAM_ID    "xxxxx"
-#define EIOT_CLOUD_HUM_INSTANCE_PARAM_ID     "xxxxx"
-#define REPORT_INTERVAL 60 // in sec
+// Initialize DHT sensor - adafruit note
+// NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
+// you need to increase the threshold for cycle counts considered a 1 or 0.
+// You can do this by passing a 3rd parameter for this threshold.  It's a bit
+// of fiddling to find the right value, but in general the faster the CPU the
+// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+// Arduino Due that runs at 84mhz a value of 30 works.
+// This is for the ESP8266 processor on ESP-01
+DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
 
+float humidity, temperature;  // Values read from sensor
+unsigned long previousMillis = 0;        // will store last temp was read
+const long interval = 2000;              // interval at which to read sensor
 
-#define EIOT_CLOUD_ADDRESS     "cloud.iot-playground.com"
-#define EIOT_CLOUD_PORT        40404
+const char* host = "hgwx.hopto.org"; // Your domain  
+String path = "/py-cgi/sender.py";
+String sensorId = "?sensorid=esp8266_001"; // uniq sensor ID
+String tempKey = "&temp=";
+String humKey = "&hum=";
 
+const char* ssid = "";
+const char* pass = "";
 
-
-#define DHT22_PIN 2  // DHT22 pin
-
-
-float oldTemp;
-float oldHum;
-
-DHT dht;
-
-
-void setup() {
+void setup(void){
   Serial.begin(115200);
+  Serial.println("");
   
-  wifiConnect();
-
-  Serial.println();
-  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
-
-  dht.setup(DHT22_PIN); // data pin 2
-  
-  oldTemp = -1;
-  oldHum = -1;
-}
-
-void loop() {
-  delay(dht.getMinimumSamplingPeriod());
-  
-  float hum = dht.getHumidity();
-  float temp = dht.getTemperature();
-
-  Serial.print(dht.getStatusString());
-  Serial.print("\t");
-  Serial.print(hum, 1);
-  Serial.print("\t\t");
-  Serial.print(temp, 1);
-  Serial.print("\t\t");
-  Serial.println(dht.toFahrenheit(temp), 1);
-
-  if (temp != oldTemp)
-  {
-    sendTeperature(temp);
-    oldTemp = temp;
-  }
-
-  if (hum != oldHum)
-  {
-    sendHumidity(hum);
-    oldHum = hum;
-  }
-
-  int cnt = REPORT_INTERVAL;
-  
-  while(cnt--)
-    delay(1000);
-}
-
-void wifiConnect()
-{
-    Serial.print("Connecting to AP");
-    WiFi.begin(AP_SSID, AP_PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+  WiFi.begin(ssid, pass);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
     Serial.print(".");
   }
   
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  dht.begin();           // initialize temperature sensor
+  Serial.println("\n\r \n\rDHT done");
+  gettemperature();
+  Serial.println(String(temperature));
+
+  delay(2);
 }
 
-void sendTeperature(float temp)
-{  
-   WiFiClient client;
-   
-   while(!client.connect(EIOT_CLOUD_ADDRESS, EIOT_CLOUD_PORT)) {
-    Serial.println("connection failed");
-    wifiConnect(); 
+void gettemperature() {
+  // Wait at least 2 seconds seconds between measurements.
+  // if the difference between the current time and last time you read
+  // the sensor is bigger than the interval you set, read the sensor
+  // Works better than delay for things happening elsewhere also
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor
+    previousMillis = currentMillis;
+
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    humidity = dht.readHumidity();          // Read humidity (percent)
+    //temperature = dht.readTemperature(true);     // Read temperature as Fahrenheit
+    temperature = dht.readTemperature();     // Read temperature as *C
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
   }
-
-  String url = "";
-  // URL: /RestApi/SetParameter/[instance id]/[parameter id]/[value]
-  url += "/RestApi/SetParameter/"+ String(EIOT_CLOUD_TEMP_INSTANCE_PARAM_ID) + "/"+String(temp); // generate EasIoT cloud update parameter URL
-
-  Serial.print("POST data to URL: ");
-  Serial.println(url);
-  
-  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
-               "Host: " + String(EIOT_CLOUD_ADDRESS) + "\r\n" + 
-               "Connection: close\r\n" + 
-               "Content-Length: 0\r\n" + 
-               "\r\n");
-
-  delay(100);
-    while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("Connection closed");
 }
 
-void sendHumidity(float hum)
-{  
-   WiFiClient client;
-   
-   while(!client.connect(EIOT_CLOUD_ADDRESS, EIOT_CLOUD_PORT)) {
+void loop() {
+
+  gettemperature();
+
+  WiFiClient client;
+  const int httpPort = 1334;
+  if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
-    wifiConnect(); 
+    return;
   }
 
-  String url = "";
-  // URL: /RestApi/SetParameter/[instance id]/[parameter id]/[value]
-  url += "/RestApi/SetParameter/"+ String(EIOT_CLOUD_HUM_INSTANCE_PARAM_ID) + "/"+String(hum); // generate EasIoT cloud update parameter URL
+  client.print(String("GET ") + path + sensorId + tempKey + temperature + humKey + humidity + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" + 
+                "Connection: keep-alive\r\n\r\n");
 
-  Serial.print("POST data to URL: ");
-  Serial.println(url);
-  
-  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
-               "Host: " + String(EIOT_CLOUD_ADDRESS) + "\r\n" + 
-               "Connection: close\r\n" + 
-               "Content-Length: 0\r\n" + 
-               "\r\n");
-
-  delay(100);
-    while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("Connection closed");
+  int cnt = REPORT_INTERVAL;  
+  while(cnt--)
+    delay(1000);
 }
